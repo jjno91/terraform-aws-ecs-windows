c01393c172995cb0cc4b543bb44c9c65a930f097
Jeremiah Norris <jjno91@gmail.com>
2018-11-06 15:11:37 -0600

    example variable re-ordering

--- examples/example.tf
+++ examples/example.tf
@@ -12,7 +12,7 @@ locals {
 
 module "ecs_windows" {
   source = "github.com/jjno91/terraform-aws-ecs-windows?ref=master"
-  vpc_id = "my_vpc"
   env    = "${var.env}"
+  vpc_id = "my_vpc"
   tags   = "${local.tags}"
 }

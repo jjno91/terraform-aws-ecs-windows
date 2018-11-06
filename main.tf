0de407735314ca7b9aa6949c2eb44802b6d02ea1
Jeremiah Norris <jjno91@gmail.com>
2018-11-06 14:57:17 -0600

    correct typo in scale up policy

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ resource "aws_autoscaling_group" "this" {
 #################################################
 
 resource "aws_autoscaling_policy" "up" {
-  name                   = "${var.env}-ecs-windows-scale-down"
+  name                   = "${var.env}-ecs-windows-scale-up"
   scaling_adjustment     = "${var.scaling_adjustment}"
   adjustment_type        = "ChangeInCapacity"
   cooldown               = "${var.scaling_cooldown}"
